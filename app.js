286b00462676f6db5df82bd363e9528c6d91d7af
Shaharyar-S-Rehman <64689533+Shaharyar-S-Rehman@users.noreply.github.com>
2021-06-05 20:12:26 +0500

    DOM Assignment

---
+++ app.js
@@ -0,0 +1,82 @@
+// i. Get element of id “main-content” and assign them in a variable.
+
+// var a = document.getElementById("main-content")
+// console.log(a)
+
+// ii. Display all child elements of “main-content” element.
+
+// var a = document.getElementById("main-content").childNodes
+// console.log(a)
+
+
+// iii. Get all elements of class “render” and show their innerHTML
+// in browser.
+
+// var r= document.getElementsByClassName("render").innerHtml="hello world";
+// console.log(r)
+
+
+// iv. Fill input value whose element id first-name using javascript.
+
+// var f= document.getElementById("first-name");
+// var p=document.createTextNode("Shaharyar")
+// f.appendChild(p)
+// console.log(f)
+
+
+
+// v. Repeat part iv for id ”last-name” and “email”.
+
+
+
+// var l= document.getElementById("last-name");
+// var p=document.createTextNode("Rehman");
+// var c = document.getElementById("email")
+// var g=document.createTextNode("shaharyarrehman79@gmail.com");
+// l.appendChild(p)
+// c.appendChild(g)
+// console.log(l)
+// console.log(c)
+
+
+
+// i. What is node type of element having id “form-content”.
+
+// var a = document.getElementById("form-content")
+// var b = a.nodeType
+// console.log(b)
+
+// ii. Show node type of element having id “lastName” and its child node.
+
+// var a =document.getElementById("lastName")
+// var b = a.nodeType
+// console.log(b)
+// var c = a.childNodes
+// console.log(c)
+
+// iii. Update child node of element having id “lastName”.
+
+
+
+// iv. Get First and last child of id “main-content”.
+ 
+// var a = document.getElementById("main-content")
+// var b = a.firstChild
+// console.log(b)
+// var c = a.lastChild
+// console.log(c)
+
+// v. Get next and previous siblings of id “lastName”.
+
+// var a = document.getElementById("lastName")
+// var b = a.previousSibling
+// var c = a.nextSibling
+// console.log(b,c)
+
+// vi. Get parent node and node type of element having id “email”
+
+// var a = document.getElementById("email")
+// var b = a.parentNode
+// console.log(b)
+// var c = a.nodeType
+// console.log(c)
